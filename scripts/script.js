2ee5d070d056c900da4e6e22b6dbb06bd8594329
Richard Cranwell <rickycranwell@gmail.com>
2021-07-05 17:53:10 -0500

    add cell selectors to displaycontroller

--- scripts/script.js
+++ scripts/script.js
@@ -6,17 +6,48 @@ const Gameboard = (() =>{
     }*/
 
     let board = [1,1,1,1,null,-1,-1,-1,1];
+    return {board};
+})();
+
+const DisplayController = (() => {
+
+    const cells = [... document.querySelectorAll("div.game-cell")];
+    const infoPanel = document.querySelector("#setup-panel");
+
+
+    let cellCount = 0;
+
+    const getSymbol = value =>{
+        if(value < 0)
+            return 'X';
+        else if(value > 0)
+            return 'O';
+        else
+            return '';
+    };
+
+
+    const cellTexts = cells.map(cell => {
+        cell.setAttribute("data-index", cellCount++);
+        return cell.children[0];
+    })
+
+    const updateCells = board => {
+        console.table(board);
+        for (let i = 0; i < 9; i++){
+            cellTexts[i].innerText = getSymbol(board[i]);
+        }
+    }
+
+    return{updateCells};
+})();
+
+const GameLogic = (()=> {
     return {};
 })();
 
-const displayController = (() => {
-    return {};
-})();
-
-const gameLogic = (()=> {
-    return {};
-})();
-
-const playerFactory = (symbol) =>{
+const PlayerFactory = (symbol) =>{
     return {symbol};
 };
+
+//DisplayController.updateCells(Gameboard.board);
