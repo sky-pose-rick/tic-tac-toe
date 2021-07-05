2c5d9aff0bbd337ecce2476dfe5aa709046fbaee
Richard Cranwell <rickycranwell@gmail.com>
2021-07-05 16:16:44 -0500

    create factory and module stubs

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,22 @@
 const Gameboard = (() =>{
-    let board = [];
+    /*let board = [];
     for(let i = 0; i < 9; i++)
     {
         board.push(null);
-    }
-    
+    }*/
+
+    let board = [1,1,1,1,null,-1,-1,-1,1];
     return {};
 })();
+
+const displayController = (() => {
+    return {};
+})();
+
+const gameLogic = (()=> {
+    return {};
+})();
+
+const playerFactory = (symbol) =>{
+    return {symbol};
+};
