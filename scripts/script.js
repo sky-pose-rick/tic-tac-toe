8bcea699b7a1ea943a97d5fafa2f0ca553419e15
Richard Cranwell <rickycranwell@gmail.com>
2021-07-07 14:50:11 -0500

    add win detection for the board

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,54 @@ const Gameboard = (() =>{
         board = [];
         for(let i = 0; i < 9; i++)
         {
-            board.push(null);
+            board.push(0);
         }
     };
 
+    const getSymbol = index =>{
+        if(board[index] < 0)
+            return 'X';
+        else if(board[index] > 0)
+            return 'O';
+        else
+            return '';
+    };
+
+    const placeSymbol = (elem, player) => {
+        let index = elem.getAttribute("data-index");
+        board[i] = player.marker;
+    };
+
+    const detectWin = (player) => {
+        //3 rows, 3 cols, 2 diagonals as win conditions
+        //diagonals are {(0,0),(1,1),(2,2)} and {(0,2),(1,1),(2,0)}
+        let counters = [];
+        for(let i = 0; i < 8; i++)
+            counters.push(0);
+        for(let x = 0; x < 3; x++){
+            for (let y = 0; y < 3; y++){
+                let index = x*3+y;
+                let value = board[index];
+                //row
+                counters[x] += value;
+                //col
+                counters[3+y] += value;
+                //diagonals
+                if(x+y === 2)
+                    counters[6] += value;
+                if(x===y)
+                    counters[7] += value;
+            }
+        }
+
+        //find any 3 in a row
+        const win = count => (player.marker * count === 3);
+        return counters.some(win);
+    }
+
     resetBoard();
     board = [1,1,1,1,null,-1,-1,-1,1];
-    return {board, resetBoard};
+    return {board, resetBoard, placeSymbol, detectWin};
 })();
 
 const DisplayController = (() => {
@@ -22,16 +63,6 @@ const DisplayController = (() => {
 
     let cellCount = 0;
 
-    const getSymbol = value =>{
-        if(value < 0)
-            return 'X';
-        else if(value > 0)
-            return 'O';
-        else
-            return '';
-    };
-
-
     const cellTexts = cells.map(cell => {
         cell.setAttribute("data-index", cellCount++);
         return cell.children[0];
@@ -40,7 +71,7 @@ const DisplayController = (() => {
     const updateCells = board => {
         console.table(board);
         for (let i = 0; i < 9; i++){
-            cellTexts[i].innerText = getSymbol(board[i]);
+            cellTexts[i].innerText = Gameboard.getSymbol(i);
         }
     }
 
@@ -52,7 +83,9 @@ const GameLogic = (()=> {
 })();
 
 const PlayerFactory = (symbol) =>{
-    return {symbol};
+    let marker = 0;
+    let name = 'blank';
+    return {symbol, marker, name};
 };
 
 //DisplayController.updateCells(Gameboard.board);
